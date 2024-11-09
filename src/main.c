a97c40742019d8ad8067891d87b1a9bee4997979
lu <lu.frog.wizard@gmail.com>
2024-11-09 05:31:00 -0300

    fixed some issues and implemented randomized room generation.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <ncurses.h>
 
-
+/* defining types */
 typedef struct _position {
     int y;
     int x;
@@ -19,6 +20,7 @@ typedef struct _room {
     int width;
 } room;
 
+/* defining functions */
 player * startPlayer();
 int movePlayer(position new_pos, player * user);
 
@@ -28,34 +30,70 @@ int drawRoom(room * room);
 int drawWalls();
 
 int checkTiles(position pos, char tile);
+int checkArea(position top_left, position bottom_right, char tile);
 
 int main() {
-    player * user;
-    char user_input;
-
     /* starting randomization */
-    
+    srand(time(NULL));
 
     /* starting game screen (terminal) */
     initscr();
     noecho();
+    
+    int win_row, win_col;
+    getmaxyx(stdscr, win_row, win_col);
 
     /* setting up the game */
-    /* generating rooms */
-    int room_number = 5;
-    room * rooms[5];
+    /* generating dungeon rooms */
+    int room_number = 8;
+    room * dungeon_rooms[room_number];
 
-    position room_position; room_position.y = 3; room_position.x = 10;
+    position room_position, room_end_position; 
+    int room_height, room_width;
 
-    rooms[0] = createRoom(room_position, 5, 8);
-    drawRoom(rooms[0]);
+    /* first room - hard coded */
+    room_position.y = 3;
+    room_position.x = 10;
+
+    dungeon_rooms[0] = createRoom(room_position, 5, 8);
+    drawRoom(dungeon_rooms[0]);
+
+    for(int i = 1; i < room_number; i++) {
+        room_height = (rand() % 7) + 4;
+        room_width = (rand() % 7) + 4;
+
+        printf("1. %d, %d\n", room_height, room_width);
+
+        room_position.y = rand() % (win_row - room_height - 2) + 1;
+        room_position.x = rand() % (win_col - room_width - 2) + 1;
+
+        room_end_position.y = room_position.y + room_height;
+        room_end_position.x = room_position.x + room_width;
+
+        printf("2. %d, %d - %d, %d\n", room_position.y, room_position.x, room_end_position.y, room_end_position.x);
+
+        if(checkArea(room_position, room_end_position, '.') == 1) {
+            printf("passou!!\n");
+            dungeon_rooms[i] = createRoom(room_position, room_height, room_width);
+            drawRoom(dungeon_rooms[i]);
+        }
+
+        else {
+            printf("não passou...\n");
+            i--;
+        }
+    }
 
     /* draws map */
     drawWalls();
 
+    /* create player character */
+    player * user;
     user = startPlayer();
 
     /* game loop */
+    char user_input;
+    
     while(user_input != 'q') {
 
         user_input = getch();
@@ -117,8 +155,9 @@ int main() {
 
     /* free all allocated memory */
     for(int i = 0; i < room_number; i++) { 
-        free(rooms[i]);
+        free(dungeon_rooms[i]);
     }
+
     free(user);
 }
 
@@ -220,5 +259,21 @@ int checkTiles(position pos, char tile) {
     }
 
     /* returns 1 if no tile found*/
+    return 1;
+}
+
+int checkArea(position top_left, position bottom_right, char tile) {
+    /* check for tile in general area */
+    for(int i = top_left.y; i < top_left.y + bottom_right.y; i++) {
+        for(int j = top_left.x; j < top_left.x + bottom_right.x; j++) {
+            if(mvinch(i, j) == tile) {
+
+                /* returns 0 if tile found */
+                return 0;
+            }
+        }
+    }
+
+    /* returns 1 if tile not found */
     return 1;
 }
